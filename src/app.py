50b474bbc4253b16b0f97b7d63bdcd7efa90214a
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-11-23 20:51:52 +0000

    guardado

--- src/app.py
+++ src/app.py
@@ -15,10 +15,15 @@ def hello_world():
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    return jsonify(todos)
 
-
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    return 'something'
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
